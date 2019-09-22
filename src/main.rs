6270c644ed779df48996c66332f79d7da02f1380
cezarmathe <cezarmathe@gmail.com>
2019-09-22 17:27:13 +0300

    Add more router params for feeds

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
 #[macro_use] extern crate serde_derive;
 #[macro_use] extern crate lazy_static;
 extern crate mongodb;
--- src/router/feed.rs
+++ src/router/feed.rs
@@ -1,5 +1,8 @@
+use crate::db::model;
+use rocket_contrib::json::Json;
+
 #[get("/feeds/<uuid>")]
-pub fn get_feed(uuid: String) {
+pub fn get_feed(uuid: String) -> Json(model::Feed) {
     unimplemented!();
 }
 
@@ -13,13 +16,13 @@ pub fn get_feed_checksum(uuid: String) {
     unimplemented!();
 }
 
-#[post("/feeds")]
-pub fn create_feed() {
+#[post("/feeds", data=<feed>)]
+pub fn create_feed(feed: model::Feed) {
     unimplemented!();
 }
 
-#[put("/feeds/<uuid>")]
-pub fn update_feed(uuid: String) {
+#[put("/feeds/<uuid>", data=<feed>)]
+pub fn update_feed(uuid: String, feed: model::Feed) {
     unimplemented!();
 }
 
