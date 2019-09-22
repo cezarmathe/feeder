f81c53248b183e763fd105c71fb9e8735c7ca6d6
cezarmathe <cezarmathe@gmail.com>
2019-09-22 13:00:17 +0300

    Add "extern crate" statements for macros

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
 mod db;
 
 fn main() {
