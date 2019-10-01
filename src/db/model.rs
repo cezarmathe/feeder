f588b30364bbf3045d191a967345e5018fa19e65
cezarmathe <cezarmathe@gmail.com>
2019-10-01 09:48:55 +0300

    Add basic implementation for a feed which can contain either uuids or items

--- src/db/model.rs
+++ src/db/model.rs
@@ -1,4 +1,4 @@
-use crate::{common::error::Error, create_error};
+use crate::{common::error::Error, create_error, db::FeederDbConn};
 
 use std::{option::Option, vec::Vec};
 
@@ -8,6 +8,14 @@ use mongodb::{coll::options::IndexModel, oid::ObjectId};
 use uuid::Uuid;
 
 const SCOPE: &str = "database/model";
+
+/// Enum that specifies whether a feed contains only
+/// the Uuids or the full items
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub enum ItemsVec {
+    Uuid(Vec<Uuid>),
+    Full(Vec<FeedItem>),
+}
 
 #[derive(Clone, Debug, Deserialize, Model, Serialize)]
 pub struct Feed {
@@ -30,7 +38,7 @@ pub struct Feed {
     pub language: Option<String>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub items_uuid: Option<Vec<Uuid>>,
+    pub items: Option<ItemsVec>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
     checksum: Option<String>,
@@ -58,12 +66,12 @@ impl Feed {
             copyright: Option::None,
             image: Option::None,
             language: Option::None,
-            items_uuid: Option::Some(Vec::new()),
+            items: Option::None,
             checksum: Option::None,
         };
 
         // compute the checksum
-        if !feed.compute_checksum() {
+        if !feed.compute_checksum(Option::None) {
             error!("checksum could not computed");
             return Result::Err(create_error!(
                 SCOPE,
@@ -75,19 +83,30 @@ impl Feed {
         return Result::Ok(feed);
     }
 
+    /// Get the UUID of this feed
     pub fn get_uuid(&self) -> Option<Uuid> {
         return self.uuid.clone();
     }
 
+    /// Get the checksum of this feed
     pub fn get_checksum(&self) -> Option<String> {
         return self.checksum.clone();
     }
 
-    /// Compute the checksum of this feed.
-    /// The checksum is saved inside the object.
-    fn compute_checksum(&mut self) -> bool {
+    /// Compute the checksum of this feed
+    /// The checksum is saved inside the object
+    fn compute_checksum(&mut self, db_conn: Option<FeederDbConn>) -> bool {
         debug!("computing checksum for feed {:?}", self);
-        let json_result = serde_json::to_string(self);
+
+        let feed: Feed;
+
+        if db_conn.is_some() {
+            feed = self.clone().with_items(db_conn.unwrap());
+        } else {
+            feed = self.clone();
+        }
+
+        let json_result = serde_json::to_string(&feed);
 
         match json_result {
             Ok(json) => {
@@ -99,8 +118,57 @@ impl Feed {
                 return true;
             }
             Err(e) => {
-                warn!("could not convert feed {:?} to json: {:?}", self, e);
+                warn!("could not convert feed {:?} to json: {:?}", feed, e);
                 return false;
+            }
+        }
+    }
+
+    /// Return this feed along with its items
+    fn with_items(mut self, db_conn: FeederDbConn) -> Self {
+        if self.items.is_none() {
+            return self;
+        }
+
+        let items_vec: ItemsVec = self.clone().items.unwrap();
+        match items_vec {
+            ItemsVec::Full(_) => self,
+            ItemsVec::Uuid(items) => {
+                let mut items_full: Vec<FeedItem> = Vec::new();
+
+                //                for item in items {
+                //
+                //                }
+
+                self
+            }
+        }
+    }
+
+    /// Return this feed with the items having only uuids
+    fn with_uuids(mut self) -> Self {
+        // TODO add some kind of error returning
+
+        // If there are no items to be converted, return
+        if self.items.is_none() {
+            return self;
+        }
+
+        let items_vec: ItemsVec = self.clone().items.unwrap();
+        match items_vec {
+            ItemsVec::Uuid(_) => self,
+            ItemsVec::Full(items) => {
+                let mut items_uuid: Vec<Uuid> = Vec::new();
+
+                for item in items {
+                    if item.get_uuid().is_none() {
+                        continue;
+                    }
+                    items_uuid.push(item.get_uuid().unwrap());
+                }
+
+                self.items = Option::Some(ItemsVec::Uuid(items_uuid));
+                self
             }
         }
     }
@@ -123,18 +191,22 @@ pub struct FeedItem {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
     id: Option<mongodb::oid::ObjectId>,
     uuid: Option<Uuid>,
-    title: String,
-    link: String,
-    description: String,
+    pub title: String,
+    pub link: String,
+    pub description: String,
 
-    author: Option<String>,
-    comments: Option<String>,
-    enclosure: Option<FeedItemEnclosure>,
+    pub author: Option<String>,
+    pub comments: Option<String>,
+    pub enclosure: Option<FeedItemEnclosure>,
 
     checksum: Option<String>,
 }
 
-impl FeedItem {}
+impl FeedItem {
+    pub fn get_uuid(&self) -> Option<Uuid> {
+        self.uuid
+    }
+}
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct FeedItemEnclosure {
