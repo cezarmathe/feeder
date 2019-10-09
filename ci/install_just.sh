2485b3a788eb64b4e2f4d66ea9a24d83d384f070
cezarmathe <cezarmathe@gmail.com>
2019-10-09 18:57:29 +0300

    Attempt to fix ci issues

--- ci/install_just.sh
+++ ci/install_just.sh
@@ -2,15 +2,15 @@
 
 cargo install just
 
-if [[ "$?" == 0 ]]; then
+if [[ "$?" == "0" ]]; then
     exit 0
 fi
 
-if [[ "$?" == 101 ]]; then
+if [[ "$?" == "101" ]]; then
     printf "%s\n" "Just is already installed."
     exit 0
 fi
 
 printf "%s\n" "Failed to install Just."
 
-exit 1
+exit $?
