d262326c3d51fe16dbd2a2d437f80dafd3ac802b
cezarmathe <cezarmathe@gmail.com>
2019-10-14 19:57:42 +0300

    Fix methods for working with multiple feeds

--- src/db/feed_wrapper.rs
+++ src/db/feed_wrapper.rs
@@ -9,16 +9,19 @@ pub trait FeedWrapper {
     /// Create a new feed
     fn create_feed(self, feed: Feed) -> DbResult<Feed>;
 
-    // /// Create multiple feeds
-    // fn create_feeds(self: Box<Self>, feeds: Vec<model::Feed>) -> Vec<DbResult<model::Feed>> {
-    //     let mut results: Vec<DbResult<model::Feed>> = Vec::new();
+    /// Create multiple feeds
+    fn create_feeds(self, feeds: Vec<Feed>) -> Vec<DbResult<Feed>>
+    where
+        Self: Clone,
+    {
+        let mut results: Vec<DbResult<Feed>> = Vec::new();
 
-    //     for feed in feeds {
-    //         results.push(self.create_feed(feed));
-    //     }
+        for feed in feeds {
+            results.push(self.clone().create_feed(feed));
+        }
 
-    //     results
-    // }
+        results
+    }
 
     // /// Save a feed in the database. Internally used by create and update operations
     // fn save_feed(&self, db_conn: &DbConnection, feed: model::Feed) -> DbResult<model::Feed>;
@@ -41,56 +44,68 @@ pub trait FeedWrapper {
     /// Get a feed from the database
     fn get_feed(self, uuid: Uuid) -> DbResult<Feed>;
 
-    // /// Get multiple feeds from the database
-    // fn get_feeds(self: Box<Self>, uuids: Vec<Uuid>) -> Vec<DbResult<model::Feed>> {
-    //     let mut results: Vec<DbResult<model::Feed>> = Vec::new();
+    /// Get multiple feeds from the database
+    fn get_feeds(self, uuids: Vec<Uuid>) -> Vec<DbResult<Feed>>
+    where
+        Self: Clone,
+    {
+        let mut results: Vec<DbResult<Feed>> = Vec::new();
 
-    //     for _uuid in uuids {
-    //         results.push(self.get_feed(_uuid));
-    //     }
+        for _uuid in uuids {
+            results.push(self.clone().get_feed(_uuid));
+        }
 
-    //     results
-    // }
+        results
+    }
 
     /// Update a feed
     fn update_feed(self, feed: Feed) -> DbResult<Feed>;
 
-    // /// Update multiple feeds
-    // fn update_feeds(self: Box<Self>, feeds: Vec<model::Feed>) -> Vec<DbResult<model::Feed>> {
-    //     let mut results: Vec<DbResult<model::Feed>> = Vec::new();
+    /// Update multiple feeds
+    fn update_feeds(self, feeds: Vec<Feed>) -> Vec<DbResult<Feed>>
+    where
+        Self: Clone,
+    {
+        let mut results: Vec<DbResult<Feed>> = Vec::new();
 
-    //     for feed in feeds {
-    //         results.push(self.update_feed(feed));
-    //     }
+        for feed in feeds {
+            results.push(self.clone().update_feed(feed));
+        }
 
-    //     results
-    // }
+        results
+    }
 
     /// Delete a feed
     fn delete_feed(self, _uuid: Uuid) -> DbResult<Report<String>>;
 
-    // /// Delete multiple feed
-    // fn delete_feeds(self: Box<Self>, uuids: Vec<Uuid>) -> Vec<DbResult<Report<String>>> {
-    //     let mut results: Vec<DbResult<Report<String>>> = Vec::new();
+    /// Delete multiple feed
+    fn delete_feeds(self, uuids: Vec<Uuid>) -> Vec<DbResult<Report<String>>>
+    where
+        Self: Clone,
+    {
+        let mut results: Vec<DbResult<Report<String>>> = Vec::new();
 
-    //     for _uuid in uuids {
-    //         results.push(self.delete_feed(_uuid));
-    //     }
+        for _uuid in uuids {
+            results.push(self.clone().delete_feed(_uuid));
+        }
 
-    //     results
-    // }
+        results
+    }
 
     /// Get the checksum of a feed
     fn get_feed_checksum(self, _uuid: Uuid) -> DbResult<String>;
 
-    // /// Get the checksum of multiple feeds
-    // fn get_feeds_checksum(self: Box<Self>, uuids: Vec<Uuid>) -> Vec<DbResult<String>> {
-    //     let mut results: Vec<DbResult<String>> = Vec::new();
+    /// Get the checksum of multiple feeds
+    fn get_feeds_checksum(self: Self, uuids: Vec<Uuid>) -> Vec<DbResult<String>>
+    where
+        Self: Clone,
+    {
+        let mut results: Vec<DbResult<String>> = Vec::new();
 
-    //     for _uuid in uuids {
-    //         results.push(self.get_feed_checksum(_uuid));
-    //     }
+        for _uuid in uuids {
+            results.push(self.clone().get_feed_checksum(_uuid));
+        }
 
-    //     results
-    // }
+        results
+    }
 }
