327df4fc6329d7881d0a982d3a7d829299d593eb
cezarmathe <cezarmathe@gmail.com>
2019-10-18 16:17:17 +0300

    Update the test_docker script

--- ci/test_docker.sh
+++ ci/test_docker.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-CONTAINER_ID=$(docker run --rm -d -p 8000:8000 feeder:$1)
+function killer() {
+    sleep 1
+    local container_id=$(docker container ls --format "{{.ID}}")
+    echo "Container id: ${container_id}"
+    sleep 5
+    docker kill ${container_id}
+}
+killer &
 
-sleep 5
-
-docker kill ${CONTAINER_ID}
+docker run --rm -p 8000:8000 feeder:$1
+if [[ "$?" == 0 ]]; then
+    exit 0
+fi
+exit 1
