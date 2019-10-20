9cccb338663f8379c176d4607d96ad9618c08ff4
cezarmathe <cezarmathe@gmail.com>
2019-10-20 14:22:24 +0300

    Fix shellcheck issues

--- ci/test_docker.sh
+++ ci/test_docker.sh
@@ -2,15 +2,17 @@
 
 function killer() {
     sleep 1
-    local container_id=$(docker container ls --format "{{.ID}}")
+    local container_id
+    container_id=$(docker container ls --format "{{.ID}}")
     echo "Container id: ${container_id}"
     sleep 5
-    docker kill ${container_id}
+    docker kill "${container_id}"
 }
 killer &
 
-docker run --rm -p 8000:8000 feeder:$1
-if [[ "$?" == 0 ]]; then
+
+if ! docker run --rm -p 8000:8000 feeder:"$1"
+then
     exit 0
 fi
 exit 1
